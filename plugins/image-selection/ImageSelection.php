04c04b3c12661705da488f1828c7f4bb42864f04
Andrew Groff <andyg@rescuescg.com>
2016-03-09 04:48:42 -0800

    plugin system work

---
+++ plugins/image-selection/ImageSelection.php
@@ -0,0 +1,12 @@
+<?php
+
+use BitSurv\Plugins\BasePlugin;
+use BitSurv\Engine\PluginSystem;
+
+class ImageSelection extends BasePlugin{
+
+    public function initialize(PluginSystem $pluginSystem)
+    {
+        dbg('image selection init');
+    }
+}
